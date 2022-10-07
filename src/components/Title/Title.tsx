590e8900a43f5335bbe162972cec01aad06990a6
Alexandra <alexa.curlie@gmail.com>
2022-10-07 13:23:43 +0400

    feat: new component Title

---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Gradients } from '../../styles/Colors';
+
+type Props = {
+  title?: string,
+  subtitle?: string,
+}
+
+export const Title = ({title, subtitle}: Props) => {
+  return (
+    <StyledTitle>
+      { title && <h2 className={'title'}>{ title }</h2>}
+      { subtitle && <h3 className={'subtitle'}>{ subtitle }</h3>}
+    </StyledTitle>
+  )
+}
+
+const StyledTitle = styled.div`
+  margin: 10px auto;
+
+  & .title {
+    text-align: center;
+    font-weight: 500;
+    font-size: 45px;
+    margin-bottom: 30px;
+    text-transform: uppercase;
+    background: ${ Gradients.green };
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  & .subtitle {
+    text-align: center;
+    font-weight: 500;
+    font-size: 25px;
+    background: ${ Gradients.green };
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+`;
--- src/styles/Colors.ts
+++ src/styles/Colors.ts
@@ -15,3 +15,8 @@ export const Shadows = {
   out: `-3px -2px 4px white, 3px 2px 4px ${Colors.cloud}`,
   card: `4px 4px 6px ${Colors.cloud}`,
 }
+
+export const Gradients = {
+  green: "linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(8, 101, 60, 1) 54%, rgba(204, 226, 204, 1) 100%)",
+  brown: "linear-gradient(90deg, rgba(36, 0, 0, 1) 0%, rgba(134, 118, 100, 1) 34%, rgba(145, 164, 162, 1) 66%",
+}
