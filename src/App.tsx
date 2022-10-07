5fc233a5131f939006bdc6837f25c5006c5b097b
Alexandra <alexa.curlie@gmail.com>
2022-10-07 13:30:37 +0400

    refactoring: small code refreshes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './redux';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
 import { CatalogPage } from './pages/CatalogPage';
-import { Provider } from 'react-redux';
-import { store } from './redux';
 
 const App = () => {
   return (
